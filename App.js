e6acdb57f32a56993e2f65e787ebcebebd7457bf
skdding3 <skdding3@gmail.com>
2022-11-08 21:13:51 +0900

    app 컴포넌트

---
+++ App.js
@@ -0,0 +1,10 @@
+export default function App({ target }) {
+    this.state = {
+        fetchedLanguage: [],
+        SelectedLanguage: []
+    }
+
+    this.setState = (nextState) => {
+        // TODO: 구현예정
+    }
+}
